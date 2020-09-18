e56f96340dfb4633f2a2b947ef1bddab5cb61f9f
Aaron Sun <aaronsun@live.com>
2020-09-18 11:23:59 -0700

    Can transition between multiple pages

--- ListifyFrontend/app/src/main/java/com/example/listifyfrontend/LoginPage.java
+++ ListifyFrontend/app/src/main/java/com/example/listifyfrontend/LoginPage.java
@@ -1,5 +1,27 @@
 package com.example.listifyfrontend;
 
-public class LoginPage {
-    //
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginPage extends AppCompatActivity {
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        button = (Button) findViewById(R.id.button3);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginPage.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
--- ListifyFrontend/app/src/main/java/com/example/listifyfrontend/SignupPage.java
+++ ListifyFrontend/app/src/main/java/com/example/listifyfrontend/SignupPage.java
@@ -1,5 +1,27 @@
 package com.example.listifyfrontend;
 
-public class SignupPage {
-    //
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignupPage extends AppCompatActivity {
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        button = (Button) findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignupPage.this, LoginPage.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
