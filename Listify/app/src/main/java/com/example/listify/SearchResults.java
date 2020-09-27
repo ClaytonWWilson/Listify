9d41a3078c72955499914b720c5eea4af395b7ee
Clayton Wilson <claytonwwilson97@gmail.com>
2020-09-26 22:56:22 -0400

    Search results layout fixes and list sorting function

--- Listify/app/src/main/java/com/example/listify/SearchResults.java
+++ Listify/app/src/main/java/com/example/listify/SearchResults.java
@@ -1,5 +1,4 @@
 package com.example.listify;
-import android.media.Image;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -16,12 +15,14 @@ import com.example.listify.adapter.SearchResultsListAdapter;
 import com.example.listify.model.Product;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class SearchResults extends AppCompatActivity {
     private ListView listView;
     private SearchResultsListAdapter searchResultsListAdapter;
-    private List<Product> productList = new ArrayList<>();
+    private List<Product> resultsProductList = new ArrayList<>();
+    private List<Product> resultsProductListFiltered = new ArrayList<>();
 
 
     @Override
@@ -62,12 +63,12 @@ public class SearchResults extends AppCompatActivity {
         });
 
         listView = (ListView) findViewById(R.id.search_results_list);
-        searchResultsListAdapter = new SearchResultsListAdapter(this, productList);
+        searchResultsListAdapter = new SearchResultsListAdapter(this, resultsProductListFiltered);
         listView.setAdapter(searchResultsListAdapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(SearchResults.this, productList.get(position).getItemName(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(SearchResults.this, resultsProductListFiltered.get(position).getItemName(), Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -75,9 +76,7 @@ public class SearchResults extends AppCompatActivity {
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
-
                 doSearch(query);
-                // TODO: Display the search results listview
                 return false;
             }
 
@@ -96,23 +95,85 @@ public class SearchResults extends AppCompatActivity {
         overridePendingTransition(R.anim.enter_from_right, R.anim.exit_from_right);
     }
 
-    // TODO: This function will handle the search operation with the database and return
-    // a listview to caller to be displayed
-    private ListView doSearch(String query) {
-        // Hardcode some search results...
-        Product a = new Product("Bottled Water", "0000", "Walmart", "0001", "0123456780", "Bro, it's water...", "Grocery", "$13.37", "9/24/2020", "1", "http://3.bp.blogspot.com/-MfroPPQVDKo/UyhUZWqGvkI/AAAAAAAAB-I/DGk622onsvc/s1600/lettuce-b-kool-cat-meme.jpg");
-        Product b = new Product("Tin Foil", "0001", "Walmart", "0001", "0123456781", "Not aluminum foil", "Grocery", "$1.00", "9/24/2020", "1", "https://i.ytimg.com/vi/q9N1doYMxR0/maxresdefault.jpg");
-        Product c = new Product("Lettuce", "0002", "Walmart", "0001", "0123456782", "It's still wet", "Grocery", "$0.60", "9/24/2020", "1", "https://www.cattitudedaily.com/wp-content/uploads/2019/12/white-cat-meme-feature.jpg");
-        Product d = new Product("Video Game", "0003", "Walmart", "0001", "0123456783", "Fun Vidya Gaemz", "Electronics", "$60.00", "9/24/2020", "1", "https://i1.wp.com/bestlifeonline.com/wp-content/uploads/2018/06/cat-meme-67.jpg?resize=1024%2C1024&ssl=1");
-        Product e = new Product("Mountain Dew", "0004", "Walmart", "0001", "0123456784", "Gamer fuel", "Grocery", "$5.87", "9/24/2020", "1", "https://memeguy.com/photos/images/gaming-cat-7680.png");
-        Product f = new Product("Tire", "0005", "Walmart", "0001", "0123456785", "30 inch rims", "Automotive", "$146.97", "9/24/2020", "1", "http://cdn.sheknows.com/articles/2013/05/pet5.jpg");
-        productList.add(a);
-        productList.add(b);
-        productList.add(c);
-        productList.add(d);
-        productList.add(e);
-        productList.add(f);
+    private void doSearch(String query) {
+        // TODO: Query Database
+        // TODO: Create a new Product Object for each result
+        // TODO: Add each result to productList
 
-        return null;
+        // Hardcode some search results...
+        for (int i = 0; i < 2; i++) {
+            Product a = new Product("Bottled Water", "0000", "Walmart", "0001", "0123456780", "Bro, it's water...", "Grocery", 13.37, "9/24/2020", "1", "http://3.bp.blogspot.com/-MfroPPQVDKo/UyhUZWqGvkI/AAAAAAAAB-I/DGk622onsvc/s1600/lettuce-b-kool-cat-meme.jpg");
+            Product b = new Product("Tin Foil", "0001", "Walmart", "0001", "0123456781", "Not aluminum foil", "Grocery", 1.00, "9/24/2020", "1", "https://i.ytimg.com/vi/q9N1doYMxR0/maxresdefault.jpg");
+            Product c = new Product("Lettuce", "0002", "Walmart", "0001", "0123456782", "Burger King foot lettuce", "Grocery", 0.60, "9/24/2020", "1", "https://www.cattitudedaily.com/wp-content/uploads/2019/12/white-cat-meme-feature.jpg");
+            Product d = new Product("Video Game", "0003", "Walmart", "0001", "0123456783", "Fun Vidya Gaemz", "Electronics", 60.00, "9/24/2020", "1", "https://i1.wp.com/bestlifeonline.com/wp-content/uploads/2018/06/cat-meme-67.jpg?resize=1024%2C1024&ssl=1");
+            Product e = new Product("Mountain Dew", "0004", "Walmart", "0001", "0123456784", "Gamer fuel", "Grocery", 5.87, "9/24/2020", "1", "https://memeguy.com/photos/images/gaming-cat-7680.png");
+            Product f = new Product("Tire", "0005", "Walmart", "0001", "0123456785", "30 inch rims", "Automotive", 146.97, "9/24/2020", "1", "http://cdn.sheknows.com/articles/2013/05/pet5.jpg");
+            resultsProductList.add(a);
+            resultsProductList.add(b);
+            resultsProductList.add(c);
+            resultsProductList.add(d);
+            resultsProductList.add(e);
+            resultsProductList.add(f);
+        }
+
+        // Add all results to the filtered list
+        resultsProductListFiltered.addAll(resultsProductList);
+    }
+
+    // Sorts the search results
+    private void sortResults(int sortMode, boolean descending) {
+        // Sort Modes
+        // 0 itemName
+        // 1 price
+        // 2 chainName
+        // 3 upc
+
+        // Sort based on mode
+        switch (sortMode) {
+            case 0:
+                resultsProductListFiltered.sort(new Comparator<Product>() {
+                    @Override
+                    public int compare(Product a, Product b) {
+                        return a.getItemName().compareToIgnoreCase(b.getItemName());
+                    }
+                });
+                break;
+
+            // TODO: May need to change this depending on if price is stored as a string or a double
+            case 1:
+                resultsProductListFiltered.sort(new Comparator<Product>() {
+                    @Override
+                    public int compare(Product a, Product b) {
+                        return (int)(a.getPrice() - b.getPrice());
+                    }
+                });
+                break;
+
+            case 2:
+                resultsProductListFiltered.sort(new Comparator<Product>() {
+                    @Override
+                    public int compare(Product a, Product b) {
+                        return a.getChainName().compareToIgnoreCase(b.getChainName());
+                    }
+                });
+                break;
+
+            case 3:
+                resultsProductListFiltered.sort(new Comparator<Product>() {
+                    @Override
+                    public int compare(Product a, Product b) {
+                        return a.getUpc().compareToIgnoreCase(b.getUpc());
+                    }
+                });
+                break;
+        }
+
+        if (descending) {
+            for (int i = 0; i < resultsProductListFiltered.size() / 2; i++) {
+                Product temp = resultsProductListFiltered.get(i);
+                resultsProductListFiltered.set(i, resultsProductListFiltered.get(resultsProductListFiltered.size() - i - 1));
+                resultsProductListFiltered.set(resultsProductListFiltered.size() - i - 1, temp);
+            }
+        }
     }
 }
