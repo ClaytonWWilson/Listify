b269bc6e78e44ced61dfeed9255d846c95d134aa
Clayton Wilson <claytonwilson@protonmail.com>
2020-10-03 21:50:21 -0400

    Item details layout

--- Listify/app/src/main/java/com/example/listify/ItemDetails.java
+++ Listify/app/src/main/java/com/example/listify/ItemDetails.java
@@ -9,15 +9,19 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class ItemDetails extends AppCompatActivity {
     private Product curProduct;
-    private FloatingActionButton fab1;
-    private FloatingActionButton fab2;
+    private LinearLayout lin1;
+    private LinearLayout lin2;
+    private TextView tvCreateNew;
+    private TextView tvAddItem;
     private TextView tvItemName;
     private TextView tvStoreName;
     private ImageView itemImage;
+    private TextView tvItemPrice;
     private TextView tvItemDesc;
     private ImageButton backToSearchbutton;
 
@@ -33,8 +37,10 @@ public class ItemDetails extends AppCompatActivity {
         curProduct = (Product) getIntent().getSerializableExtra("SelectedProduct");
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab1 = (FloatingActionButton) findViewById(R.id.fab1);
-        fab2 = (FloatingActionButton) findViewById(R.id.fab2);
+//        fab1 = (FloatingActionButton) findViewById(R.id.fab1);
+//        fab2 = (FloatingActionButton) findViewById(R.id.fab2);
+        lin1 = (LinearLayout) findViewById(R.id.lin1);
+        lin2 = (LinearLayout) findViewById(R.id.lin2);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -55,8 +61,17 @@ public class ItemDetails extends AppCompatActivity {
         tvStoreName = (TextView) findViewById(R.id.store_name);
         tvStoreName.setText(curProduct.getChainName());
 
+        tvItemPrice = (TextView) findViewById(R.id.item_price);
+        tvItemPrice.setText(String.format("$%.2f", curProduct.getPrice()));
+
         tvItemDesc = (TextView) findViewById(R.id.item_desc);
         tvItemDesc.setText(curProduct.getDescription());
+
+        tvCreateNew = (TextView) findViewById(R.id.create_new_list);
+        tvCreateNew.setVisibility(View.INVISIBLE);
+
+        tvAddItem = (TextView) findViewById(R.id.add_item_to_list);
+        tvAddItem.setVisibility(View.INVISIBLE);
 
         backToSearchbutton = (ImageButton) findViewById(R.id.back_to_search_results_button);
         backToSearchbutton.setOnClickListener(new View.OnClickListener() {
@@ -69,14 +84,18 @@ public class ItemDetails extends AppCompatActivity {
 
     private void showFABMenu(){
         isFABOpen=true;
-        fab1.animate().translationY(-getResources().getDimension(R.dimen.standard_55));
-        fab2.animate().translationY(-getResources().getDimension(R.dimen.standard_105));
+        lin1.animate().translationY(-getResources().getDimension(R.dimen.standard_55));
+        lin2.animate().translationY(-getResources().getDimension(R.dimen.standard_105));
+        tvAddItem.setVisibility(View.VISIBLE);
+        tvCreateNew.setVisibility(View.VISIBLE);
     }
 
     private void closeFABMenu(){
         isFABOpen=false;
-        fab1.animate().translationY(0);
-        fab2.animate().translationY(0);
+        lin1.animate().translationY(0);
+        lin2.animate().translationY(0);
+        tvAddItem.setVisibility(View.INVISIBLE);
+        tvCreateNew.setVisibility(View.INVISIBLE);
     }
 
 
