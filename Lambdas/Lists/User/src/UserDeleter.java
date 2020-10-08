925a249b4c728d3f77df39db537943b3cfb3cee8
Clayton Wilson <claytonwwilson97@gmail.com>
2020-10-08 12:34:14 -0400

    user delete debugging

--- Lambdas/Lists/User/src/UserDeleter.java
+++ Lambdas/Lists/User/src/UserDeleter.java
@@ -1,13 +1,9 @@
-import com.amazonaws.services.cognitoidp.AWSCognitoIdentityProvider;
-import com.amazonaws.services.cognitoidp.AWSCognitoIdentityProviderClientBuilder;
-import com.amazonaws.services.cognitoidp.model.AdminDeleteUserRequest;
-import com.amazonaws.services.cognitoidp.model.AdminUserGlobalSignOutRequest;
-
-import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Properties;
 
 public class UserDeleter implements CallHandler {
 
@@ -15,6 +11,7 @@ public class UserDeleter implements CallHandler {
     private String cognitoID;
 
     //private final String REMOVE_FROM_LIST = "DELETE FROM ListProduct WHERE (ProductID = ? AND ListID = ?);";
+    private final String GET_LISTS = "SELECT * FROM List WHERE (owner = ?);";
 
     public UserDeleter(DBConnector connector, String cognitoID) {
         this.connector = connector;
@@ -22,25 +19,45 @@ public class UserDeleter implements CallHandler {
     }
 
     public Object conductAction(Map<String, Object> bodyMap, HashMap<String, String> queryString, String cognitoID) throws SQLException {
-        AWSCognitoIdentityProvider awsCognitoIdentityProvider = AWSCognitoIdentityProviderClientBuilder.defaultClient();
-        Properties cognitoProperties;
-        try {
-            cognitoProperties = DBConnector.loadProperties("cognitoProperties.json");
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
+//        AWSCognitoIdentityProvider awsCognitoIdentityProvider = AWSCognitoIdentityProviderClientBuilder.defaultClient();
+//        Properties cognitoProperties;
+//        try {
+//            cognitoProperties = DBConnector.loadProperties("cognitoProperties.json");
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//            return null;
+//        }
+//        String userPoolId = cognitoProperties.get("userPoolId").toString();
+//        System.out.println(userPoolId);
+//        AdminUserGlobalSignOutRequest adminUserGlobalSignOutRequest = new AdminUserGlobalSignOutRequest().withUserPoolId(userPoolId);
+//        adminUserGlobalSignOutRequest.setUsername(cognitoID);
+//        System.out.println(adminUserGlobalSignOutRequest);
+//        awsCognitoIdentityProvider.adminUserGlobalSignOut(adminUserGlobalSignOutRequest);
+//        AdminDeleteUserRequest adminDeleteUserRequest = new AdminDeleteUserRequest().withUserPoolId(userPoolId);
+//        adminDeleteUserRequest.setUsername(cognitoID);
+//        System.out.println(adminDeleteUserRequest);
+//        awsCognitoIdentityProvider.adminDeleteUser(adminDeleteUserRequest);
+
+
+        Connection connection = connector.getConnection();
+//        try {
+        PreparedStatement statement = connection.prepareStatement(GET_LISTS);
+        statement.setString(1, cognitoID);
+        System.out.println(statement);
+        ResultSet userLists = statement.executeQuery();
+        while (userLists.next()) {
+            int listID = userLists.getInt("listID");
+            System.out.println(String.format("%d", listID));
         }
-        String userPoolId = cognitoProperties.get("userPoolId").toString();
-        System.out.println(userPoolId);
-        AdminUserGlobalSignOutRequest adminUserGlobalSignOutRequest = new AdminUserGlobalSignOutRequest().withUserPoolId(userPoolId);
-        adminUserGlobalSignOutRequest.setUsername(cognitoID);
-        System.out.println(adminUserGlobalSignOutRequest);
-        awsCognitoIdentityProvider.adminUserGlobalSignOut(adminUserGlobalSignOutRequest);
-        AdminDeleteUserRequest adminDeleteUserRequest = new AdminDeleteUserRequest().withUserPoolId(userPoolId);
-        adminDeleteUserRequest.setUsername(cognitoID);
-        System.out.println(adminDeleteUserRequest);
-        awsCognitoIdentityProvider.adminDeleteUser(adminDeleteUserRequest);
+
+//            connection.commit();
+//        } finally {
+//            connection.close();
+//        }
         return null;
+
+
+//        return null;
 
         //        Connection connection = connector.getConnection();
 //        try {
