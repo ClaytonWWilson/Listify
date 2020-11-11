a441b672f438d33094593bcc4c8217d1d68f883c
Aaron Sun <aaronsun@live.com>
2020-11-11 14:44:51 -0800

    Added user's email to Profile page

--- Listify/app/src/main/java/com/example/listify/ui/profile/ProfileFragment.java
+++ Listify/app/src/main/java/com/example/listify/ui/profile/ProfileFragment.java
@@ -10,6 +10,8 @@ import android.view.ViewGroup;
 import android.view.LayoutInflater;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
@@ -29,6 +31,9 @@ public class ProfileFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        TextView emailText = (TextView) root.findViewById(R.id.textViewEmail);
+        emailText.setText(am.getEmail());
 
         toDeleteAccountPage = (Button) root.findViewById(R.id.button);
         toDeleteAccountPage.setOnClickListener(new View.OnClickListener() {
