b478ace5529645b2673fdd45dd4da2060ae2dd9f
NMerz <nmerz@icloud.com>
2020-11-14 15:02:04 -0500

    Update delete to use delete permission
    Previously used list creator as filter

--- Lambdas/Lists/List/src/ListDeleter.java
+++ Lambdas/Lists/List/src/ListDeleter.java
@@ -10,7 +10,7 @@ public class ListDeleter implements CallHandler {
     private final Connection connection;
     private final String cognitoID;
 
-    private final String GET_LISTS = "SELECT * FROM List WHERE (owner = ? AND listID = ?);";
+    private final String ACCESS_CHECK = "SELECT * from ListSharee WHERE userID = ? and listID = ?;";
     private final String DELETE_LIST = "DELETE FROM List WHERE listID = ?;";
     private final String DELETE_REQUESTOR_ACCESS = "DELETE FROM ListSharee where listID = ? AND userID = ?;";
     private final String DELETE_LIST_ACCESS = "DELETE FROM ListSharee where listID = ?;";
@@ -30,7 +30,7 @@ public class ListDeleter implements CallHandler {
         System.out.println(cleanRequestorAccess);
         cleanRequestorAccess.executeUpdate();
 
-        PreparedStatement accessCheck = connection.prepareStatement(GET_LISTS);
+        PreparedStatement accessCheck = connection.prepareStatement(ACCESS_CHECK);
         accessCheck.setString(1, cognitoID);
         accessCheck.setInt(2, listID);
         System.out.println(accessCheck);
