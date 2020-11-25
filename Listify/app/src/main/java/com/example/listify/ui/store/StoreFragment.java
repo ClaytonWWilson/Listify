dc964d5cbd8bd9c37ac1317042bd8989f546e72d
Aaron Sun <aaronsun@live.com>
2020-11-24 17:56:48 -0800

    Added Stores page

---
+++ Listify/app/src/main/java/com/example/listify/ui/store/StoreFragment.java
@@ -0,0 +1,18 @@
+package com.example.listify.ui.store;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.LayoutInflater;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.listify.R;
+
+public class StoreFragment extends Fragment {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_stores, container, false);
+        return root;
+    }
+}
